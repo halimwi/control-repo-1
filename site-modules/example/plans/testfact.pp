d0248f7ea8d08a23e7b8ba8bd8d0e38365f54415
halimwi <hwijaya@gmail.com>
2023-02-09 19:35:10 +0800

    Create testfact.pp

---
+++ site-modules/example/plans/testfact.pp
@@ -0,0 +1,11 @@
+plan example::testfact (
+  TargetSpec $targets,
+) {
+  run_plan(facts, targets => $targets)
+  $centos_targets = get_targets($targets).filter |$n| { $n.facts['os']['name'] == 'CentOS' }
+  get_targets($targets).each |$target| {
+    $output = $target.facts['os']['name']
+    run_task('exec', $target, command => "echo $output")
+  }
+  run_task('exec', $centos_targets, command => "echo $targets")
+}
