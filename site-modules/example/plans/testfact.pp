e9bec819cb81b69b75c20bde2376e9bb420343a8
halimwi <hwijaya@gmail.com>
2023-02-13 15:02:19 +0800

    Update testfact.pp

--- site-modules/example/plans/testfact.pp
+++ site-modules/example/plans/testfact.pp
@@ -2,10 +2,12 @@ plan example::testfact (
   TargetSpec $targets,
 ) {
   run_plan(facts, targets => $targets)
-  $centos_targets = get_targets($targets).filter |$n| { $n.facts['os']['name'] == 'CentOS' }
+  #$centos_targets = get_targets($targets).filter |$n| { $n.facts['os']['name'] == 'CentOS' }
   get_targets($targets).each |$target| {
     $output = $target.facts['os']['name']
     run_task('exec', $target, command => "echo $output")
+    run_task('example::sendemail', 'sgsedemonix0.se.automationdemos.com', factoutput => "The fact value is $output")
   }
-  run_task('exec', $centos_targets, command => "echo $targets")
+  #run_task('exec', $centos_targets, command => "echo $targets")
+  
 }
