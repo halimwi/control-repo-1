60ddcf8caf13991e51f3fb0de46942d702c7400f
halimwi <hwijaya@gmail.com>
2023-02-13 18:31:15 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,6 +8,7 @@ node default {
   if !empty( $trusted['extensions']['pp_role'] ) {
     $role = $trusted['extensions']['pp_role']
     $project = $trusted['extensions']['pp_project']
+    include profile::platform::baseline
     include "${project}::role::${role}"
   }
 }
