79ec9ea2f7eb14b0e3173656df582eb0a4aa8129
halimwi <hwijaya@gmail.com>
2023-02-17 14:49:00 +0800

    Create environment_group.rb

---
+++ site-modules/example/lib/facter/environment_group.rb
@@ -0,0 +1,7 @@
+Facter.add(:environment_group) do
+  confine :clientcert => 'ip-10-8-0-221.ap-southeast-1.compute.internal'
+  setcode do
+    env_grp = Facter.value('Test')
+    env_grp
+  end
+end
