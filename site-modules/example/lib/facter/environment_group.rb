1c2f007a03372641c32ee34980dfb54e9324da77
halimwi <hwijaya@gmail.com>
2023-02-17 14:57:08 +0800

    Update environment_group.rb

--- site-modules/example/lib/facter/environment_group.rb
+++ site-modules/example/lib/facter/environment_group.rb
@@ -1,5 +1,5 @@
 Facter.add(:environment_group) do
-  confine :clientcert => 'ip-10-8-0-221.ap-southeast-1.compute.internal'
+#  confine :clientcert => 'ip-10-8-0-221.ap-southeast-1.compute.internal'
   setcode do
     env_grp = Facter.value('Test')
     env_grp
