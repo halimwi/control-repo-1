02e75bcc5e5366dc498ac806bef73a2e8d80c0ea
halimwi <hwijaya@gmail.com>
2023-02-28 09:47:53 +0800

    Update bnmenv.rb

--- site-modules/example/lib/facter/bnmenv.rb
+++ site-modules/example/lib/facter/bnmenv.rb
@@ -1,7 +1,8 @@
 Facter.add(:bnmenv) do
 #  confine :clientcert => 'ip-10-8-0-221.ap-southeast-1.compute.internal'
-  setcode do
-    envgrp = Facter.value('Test')
-    envgrp
-  end
+#  setcode do
+#    envgrp = Facter.value('Test')
+#    envgrp
+#  end
+  setcode 'which ruby'
 end
