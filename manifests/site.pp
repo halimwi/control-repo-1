2d7fbdddd4822b90cfa2840da41d72208d70e1de
halimwi <hwijaya@gmail.com>
2023-03-20 16:41:46 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,12 +4,12 @@
 File { backup => false }
 
 node default {
-  if !empty( $trusted['extensions']['pp_project'] ) {
-    $role = $trusted['extensions']['pp_role']
-    $project = $trusted['extensions']['pp_project']
-    include profile::platform::baseline
-    include "${project}::role::${role}"
-  }
+#  if !empty( $trusted['extensions']['pp_project'] ) {
+#    $role = $trusted['extensions']['pp_role']
+#    $project = $trusted['extensions']['pp_project']
+#    include profile::platform::baseline
+#    include "${project}::role::${role}"
+#  }
 }
 
 # Uncomment the line below to enable ServiceNow Node Classification
